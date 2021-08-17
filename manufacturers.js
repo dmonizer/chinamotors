f4f654479db3402a671f21cf7a65afc4d9a6d450
ErikS <erik.suit@gmail.com>
2021-08-17 16:50:23 +0300

    Create manufacturers.js

---
+++ manufacturers.js
@@ -0,0 +1,11 @@
+const manufacturers = {
+    DJ: "http://www.dajiangmotor.com/",
+    QJ: "Qianjiang",
+    TZH: "http://www.cqtianzhong.cn/",
+    IP: "Lifan?",
+    LC: "Loncin",
+    LX: "Loncin",
+    ZMD:"Zongshen",
+    BN:"Zhongneng Industry Group",
+    VE:"Vento"
+}
